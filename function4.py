a2b4b66d22c5677313ec543188031bbf788da4b5
papasmf1 <papasmf1@gmail.com>
2022-07-05 11:07:16 +0900

    함수 연습 완료

--- function4.py
+++ function4.py
@@ -15,5 +15,6 @@ print(userURIBuilder("ycampus.com","80", id="kim", passwd="1234",
 g = lambda x,y:x*y
 print(g(3,4))
 print(g(5,6))
-print( (lambda x:x*x)(3) )
-print(globals() )
+print((lambda x:x*x)(3))
+print(globals())
+
