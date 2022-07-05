1948c81bdba9ab963e21bbc3122cdf833563f7af
papasmf1 <papasmf1@gmail.com>
2022-07-05 14:22:26 +0900

    클래스 연습2

--- class1.py
+++ class1.py
@@ -11,5 +11,14 @@ class Person:
 
 #2)인스턴스 생성
 p1 = Person()
+p2 = Person() 
+p2.name = "전우치"
+#3)메서드 호출
 p1.print() 
+p2.print() 
 
+#런타임(실행시간)에 변수를 추가 ==> 동적 형식의 언어
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
