c6e447f970c1a4e4ea4339949cc416c2d931afc1
papasmf1 <papasmf1@gmail.com>
2022-07-06 15:06:17 +0900

    패턴 연습3

--- DemoRE.py
+++ DemoRE.py
@@ -3,13 +3,21 @@ import re
 
 #print(dir(re))
 #특정한 규칙이 있는지?
-result = re.search("[0-9]*th", "35th")
+result = re.search("[0-9]*th", "  35th")
 print(result)
 print(result.group())
 
-#match함수
-result = re.match("[0-9]*th", "35th")
-print(result)
+#match함수(함정을 추가)
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+print(bool(re.search("ap", "this is apple")))
+print(bool(re.match("ap", "this is apple")))
+
+#우편번호, 연도 검색 
+result = re.search("\d{4}", "올해는 2022년입니다.")
 print(result.group())
 
-
+result = re.search("[0-9]{5}", "우리 동네는 52300입니다.")
+print(result.group())
