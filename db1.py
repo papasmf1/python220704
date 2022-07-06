46fffac3de45c0485ed08d46ffba686dbaa152e0
papasmf1 <papasmf1@gmail.com>
2022-07-06 17:19:34 +0900

    데이터베이스 연습2

--- db1.py
+++ db1.py
@@ -1,11 +1,11 @@
 # db1.py 
 import sqlite3
 
-#메모리에서 작업
+#메모리에서 작업(약간의 약속된 문자열)
 con = sqlite3.connect(":memory:")
 #커서객체를 리턴
 cur = con.cursor()
-#데이터를 저장할 테이블 생성
+#데이터를 저장할 테이블 생성(varchar, varchar2, nvarchar, char)
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-222');")
@@ -14,4 +14,5 @@ cur.execute("insert into PhoneBook values ('derick', '010-222');")
 cur.execute("select * from PhoneBook;")
 for row in cur:
     print(row)
+    
 
