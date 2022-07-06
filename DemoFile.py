29e216db8e7e8278b3316adc5807225330831f21
papasmf1 <papasmf1@gmail.com>
2022-07-06 12:09:20 +0900

    파일 연습 2

--- DemoFile.py
+++ DemoFile.py
@@ -7,3 +7,16 @@ f.close()
 print("c:/work/test.txt")
 #raw string notation(날것 그대로 가공안함) C#(@)
 print(r"c:\work\newfile.txt")
+
+#파일 읽기
+f = open("c:\\work\\demo.txt", "rt")
+result = f.read()
+print(result)
+#파일 포인터의 위치 
+print(f.tell())
+#다시 처음으로 이동(리셋)
+f.seek(0)
+print(f.readline())
+print(f.readline())
+
+f.close() 
