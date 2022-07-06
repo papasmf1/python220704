e76be6796293378a841fabe1f0c647eb19963817
papasmf1 <papasmf1@gmail.com>
2022-07-06 12:22:37 +0900

    파일 연습 3

--- DemoFile.py
+++ DemoFile.py
@@ -4,10 +4,6 @@ f = open("c:\\work\\demo.txt", "wt")
 f.write("첫번째\n두번째\n세번째라인\n")
 f.close()
 
-print("c:/work/test.txt")
-#raw string notation(날것 그대로 가공안함) C#(@)
-print(r"c:\work\newfile.txt")
-
 #파일 읽기
 f = open("c:\\work\\demo.txt", "rt")
 result = f.read()
@@ -16,7 +12,19 @@ print(result)
 print(f.tell())
 #다시 처음으로 이동(리셋)
 f.seek(0)
-print(f.readline())
-print(f.readline())
+print(f.readline(), end="")
+print(f.readline(), end="")
+#리스트로 출력
+f.seek(0)
+lst = f.readlines() 
+print(lst)
 
 f.close() 
+print(f.closed)
+
+#기존 파일에 첨부하기(a+, wt) 
+#with키워드를 같이 쓰는 경우 
+with open("c:\\work\\demo.txt", "wt") as f:
+    f.write("새로운 데이터\n")
+
+
