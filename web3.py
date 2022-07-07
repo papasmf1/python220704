1a52654ca9c974cd2c028f99be0b22dbac971f0d
papasmf1 <papasmf1@gmail.com>
2022-07-07 13:48:19 +0900

    네이버 웹툰 파일로 저장

--- web3.py
+++ web3.py
@@ -4,17 +4,24 @@ import urllib.request
 #크롤링
 from bs4 import BeautifulSoup
 
+#파일에 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
 #수열을 생성해서 URL주소 만들기
-for i in range(1,6):
-    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
-    print(url)
-    data = urllib.request.urlopen(url)
-    soup = BeautifulSoup(data, "html.parser")
+try:
+    for i in range(1,11):
+        url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+        print(url)
+        data = urllib.request.urlopen(url)
+        soup = BeautifulSoup(data, "html.parser")
 
-    cartoons = soup.find_all("td", class_="title")
+        cartoons = soup.find_all("td", class_="title")
 
-    for item in cartoons:
-        title = item.find("a").text
-        print(title.strip())
-
-                        
+        for item in cartoons:
+            title = item.find("a").text
+            print(title.strip())
+            f.write(title + "\n")
+                            
+    f.close() 
+    print("웹 크롤링 종료")
+except:
+    pass 
